d581fe298e9dc2497c5089b03ec1734b1bd07db9
vincent devault <vincentdevault@vincents-MacBook-Pro.local>
2024-02-07 10:19:22 +0100

    feat: creation du formulaire pour poser une question

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Form\QuestionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'ask_question')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('question/index.html.twig');
+        $FormQuestion = $this->createForm(QuestionType::class);
+        $FormQuestion->handleRequest($request);
+
+        if ($FormQuestion->isSubmitted() && $FormQuestion->isValid()) {
+            dump($FormQuestion->getData());
+        }
+
+            return $this->render('question/index.html.twig', ['form' => $FormQuestion->createView()]);
     }
 }
